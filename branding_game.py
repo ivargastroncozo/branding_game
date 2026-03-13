e705d6f79d91dd483012ebc35462ba3d018259ed
Ignacio Vargas <vargastroncozoignacio@gmail.com>
2026-03-13 00:43:30 -0500

    Update branding_game.py

--- branding_game.py
+++ branding_game.py
@@ -8,7 +8,7 @@ if "brand_equity" not in st.session_state:
     st.session_state.brand_sr = 10
     st.session_state.image = 0
 
-st.write("This game is under construction send all your comments and suggestions to vargastroncozoignacio@gmail.com")
+st.write("This game is being developed by Ignacio Vargas. Please send all your comments and suggestions to vargastroncozoignacio@gmail.com")
 st.write("You are the brand manager of a new coffee brand.")
 
 st.subheader("Current metrics")
@@ -53,7 +53,7 @@ if st.button("Make decision"):
         st.session_state.brand_tom += 10
         st.session_state.brand_sr -= 10
         st.session_state.image += 10
-        st.success("Your brand is perceived as high quality, just as you wanted!")
+        st.success("Is this consistent with your essence? “Consistency” is vital!")
    
     elif choice1 == "Brewing to save the world" & choice2 == "Sustainable ethical coffee":
         st.session_state.brand_tom += 3
@@ -65,7 +65,7 @@ if st.button("Make decision"):
         st.session_state.brand_tom += 3
         st.session_state.brand_sr += 2
         st.session_state.image += 5
-        st.success("Consumers appreciate your ethical positioning, just as you wanted!.")
+        st.success("Is this consistent with your essence? “Consistency” is vital!")
 
     elif choice1 == "Good coffee for everyone" & choice2 == "Low price mass coffee":
         st.session_state.brand_tom -= 7
@@ -73,10 +73,17 @@ if st.button("Make decision"):
         st.session_state.image += 3
         st.success("Your brand gained volume but lost prestige, just as you wanted!")
 
+    elif choice1 == "Good coffee for everyone" & choice2 != "Low price mass coffee":
+        st.session_state.brand_tom -= 7
+        st.session_state.brand_sr += 5
+        st.session_state.image += 3
+        st.success("Is this consistent with your essence? “Consistency” is vital!")
+
 st.subheader("Updated results")
 
 st.write("Brand Top-of-Mind %:", st.session_state.brand_tom)
 st.write("Brand Spontaneous recall %:", st.session_state.brand_sr)
 st.write("Alignment with the desired image:",  st.session_state.image)
+
 
 
