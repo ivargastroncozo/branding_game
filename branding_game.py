41a230d92302fff75547f8205b4622e533ae4158
Ignacio Vargas <vargastroncozoignacio@gmail.com>
2026-03-13 00:50:11 -0500

    Update branding_game.py

--- branding_game.py
+++ branding_game.py
@@ -43,47 +43,39 @@ choice2 = st.radio(
 
 if st.button("Make decision"):
 
-    if choice1 == "Masterfully crafted coffee" & choice2 == "Premium artisanal coffee":
+    if choice1 == "Masterfully crafted coffee" and choice2 == "Premium artisanal coffee":
         st.session_state.brand_tom += 10
         st.session_state.brand_sr -= 10
         st.session_state.image += 10
         st.success("Your brand is perceived as high quality, just as you wanted!")
 
-    elif choice1 == "Masterfully crafted coffee" & choice2 != "Premium artisanal coffee":
-        st.session_state.brand_tom += 10
-        st.session_state.brand_sr -= 10
-        st.session_state.image += 10
-        st.success("Is this consistent with your essence? “Consistency” is vital!")
+    elif choice1 == "Masterfully crafted coffee" and choice2 != "Premium artisanal coffee":
+        st.warning("Your positioning is inconsistent with your brand essence.")
    
-    elif choice1 == "Brewing to save the world" & choice2 == "Sustainable ethical coffee":
+    elif choice1 == "Brewing to save the world" and choice2 == "Sustainable ethical coffee":
         st.session_state.brand_tom += 3
         st.session_state.brand_sr += 2
         st.session_state.image += 5
         st.success("Consumers appreciate your ethical positioning, just as you wanted!.")
         
-    elif choice1 == "Brewing to save the world" & choice2 != "Sustainable ethical coffee":
-        st.session_state.brand_tom += 3
-        st.session_state.brand_sr += 2
-        st.session_state.image += 5
-        st.success("Is this consistent with your essence? “Consistency” is vital!")
+    elif choice1 == "Brewing to save the world" and choice2 != "Sustainable ethical coffee":
+        st.warning("Your positioning is inconsistent with your brand essence.")
 
-    elif choice1 == "Good coffee for everyone" & choice2 == "Low price mass coffee":
+    elif choice1 == "Good coffee for everyone" and choice2 == "Low price mass coffee":
         st.session_state.brand_tom -= 7
         st.session_state.brand_sr += 5
         st.session_state.image += 3
         st.success("Your brand gained volume but lost prestige, just as you wanted!")
 
-    elif choice1 == "Good coffee for everyone" & choice2 != "Low price mass coffee":
-        st.session_state.brand_tom -= 7
-        st.session_state.brand_sr += 5
-        st.session_state.image += 3
-        st.success("Is this consistent with your essence? “Consistency” is vital!")
+    elif choice1 == "Good coffee for everyone" and choice2 != "Low price mass coffee":
+        st.warning("Your positioning is inconsistent with your brand essence.")
 
 st.subheader("Updated results")
 
 st.write("Brand Top-of-Mind %:", st.session_state.brand_tom)
 st.write("Brand Spontaneous recall %:", st.session_state.brand_sr)
 st.write("Alignment with the desired image:",  st.session_state.image)
+
 
 
 
