a419fe4398c2ba95c2e2a454a1de03f280e513b6
Ignacio Vargas <vargastroncozoignacio@gmail.com>
2026-03-14 01:21:45 -0500

    Update branding_game.py

--- branding_game.py
+++ branding_game.py
@@ -3,19 +3,13 @@ import streamlit as st
 st.title("🎮 Brand Strategy Game")
 
 # inicializar estado del juego
-if "brand_equity" not in st.session_state:
-    st.session_state.brand_tom = 50
-    st.session_state.brand_sr = 10
-    st.session_state.image = 0
+#if "brand_equity" not in st.session_state:
+#   st.session_state.brand_tom = 50
+#   st.session_state.brand_sr = 10
+#   st.session_state.image = 0
 
 st.write("This game is being developed by Ignacio Vargas. Please send all your comments and suggestions to vargastroncozoignacio@gmail.com")
-st.write("You are the brand manager of a new coffee brand.")
-
-st.subheader("Current metrics")
-
-st.write("Brand Top-of-Mind %:", st.session_state.brand_tom)
-st.write("Brand Spontaneous recall %:", st.session_state.brand_sr)
-st.write("Alignment with the desired image:", st.session_state.image)
+st.write("You are the brand manager of a new coffee brand, and you will need to make decisions to achieve your desired brand equity.")
 
 st.subheader("Decision 1: Brand Essence")
 
@@ -30,9 +24,36 @@ choice1 = st.radio(
 
 st.success("Remember that all your decisions must reflect your essence. This is your brand DNA!")
 
-st.subheader("Decision 2: Positioning")
+st.subheader("Now you need to define your target market. First, select your segmentation variables.")
 
 choice2 = st.radio(
+    "What will be your segmentation variables?",
+    [
+        "Age – Gender",
+        "Occupation status (Student, Worker, Unemployed) – Price sensitivity",
+        "Income – Ethnicity"
+        "Age – Physical activity (times per week)"
+    ]
+)
+
+
+if st.button("Make decision"):
+
+    if choice2 == "Age – Gender" and choice2 == "Premium artisanal coffee":
+        st.warning("Selecting two demographic variables is not a good option. It can lead to defining one segment whose members have very diverse preferences.")
+
+    elif choice2 == "Occupation status (Student, Worker, Unemployed) – Price sensitivity":
+        st.success("This variables are a good general option! Combining demographics with psychographics characteristics.")
+   
+    elif choice2 == "Income – Ethnicity":
+        st.warning("Selecting two demographic variables is not a good option. It can lead to defining one segment whose members have very diverse preferences.")
+
+    elif choice2 == "Age – Physical activity (times per week)":
+        st.warning("Here you are combining demographics and behavioral variables, but is physical activity relevant for your brand?")
+
+st.subheader("Decision 3: Positioning")
+
+choice3 = st.radio(
     "How will you position the brand?",
     [
         "Premium artisanal coffee",
@@ -43,38 +64,23 @@ choice2 = st.radio(
 
 if st.button("Make decision"):
 
-    if choice1 == "Masterfully crafted coffee" and choice2 == "Premium artisanal coffee":
-        st.session_state.brand_tom += 10
-        st.session_state.brand_sr -= 10
-        st.session_state.image += 10
+    if choice1 == "Masterfully crafted coffee" and choice3 == "Premium artisanal coffee":
         st.success("Your brand is perceived as high quality, just as you wanted!")
 
-    elif choice1 == "Masterfully crafted coffee" and choice2 != "Premium artisanal coffee":
+    elif choice1 == "Masterfully crafted coffee" and choice3 != "Premium artisanal coffee":
         st.warning("Your positioning is inconsistent with your brand essence.")
    
-    elif choice1 == "Brewing to save the world" and choice2 == "Sustainable ethical coffee":
-        st.session_state.brand_tom += 3
-        st.session_state.brand_sr += 2
-        st.session_state.image += 5
+    elif choice1 == "Brewing to save the world" and choice3 == "Sustainable ethical coffee":
         st.success("Consumers appreciate your ethical positioning, just as you wanted!.")
         
-    elif choice1 == "Brewing to save the world" and choice2 != "Sustainable ethical coffee":
+    elif choice1 == "Brewing to save the world" and choice3 != "Sustainable ethical coffee":
         st.warning("Your positioning is inconsistent with your brand essence.")
 
-    elif choice1 == "Good coffee for everyone" and choice2 == "Low price mass coffee":
-        st.session_state.brand_tom -= 7
-        st.session_state.brand_sr += 5
-        st.session_state.image += 3
+    elif choice1 == "Good coffee for everyone" and choice3 == "Low price mass coffee":
         st.success("Your brand gained volume but lost prestige, just as you wanted!")
 
-    elif choice1 == "Good coffee for everyone" and choice2 != "Low price mass coffee":
+    elif choice1 == "Good coffee for everyone" and choice3 != "Low price mass coffee":
         st.warning("Your positioning is inconsistent with your brand essence.")
-
-st.subheader("Updated results")
-
-st.write("Brand Top-of-Mind %:", st.session_state.brand_tom)
-st.write("Brand Spontaneous recall %:", st.session_state.brand_sr)
-st.write("Alignment with the desired image:",  st.session_state.image)
 
 
 
